1598e23b2297f9000f3577edd666924f55545c87
AntonReinhard <anton.reinhard@proton.me>
2025-07-08 15:39:21 +0200

    Improve tests

--- ext/DoubleFloatsExt.jl
+++ ext/DoubleFloatsExt.jl
@@ -9,6 +9,13 @@ const P = PrecisionCarrier
 function Base.convert(::Type{DoubleFloat{T}}, p::P) where {T <: Base.IEEEFloat}
     return P{DoubleFloat{T}}(convert(DoubleFloat{T}, p.x), p.big)
 end
+function Base.convert(::Type{DoubleFloat}, p::P)
+    _double(::Type{DoubleFloat{T}}) where {T <: Base.IEEEFloat} = DoubleFloat{T}
+    _double(::Type{T}) where {T <: Base.IEEEFloat} = DoubleFloat{T}
+
+    D_TYPE = _double(eltype(p))
+    return P{D_TYPE}(convert(D_TYPE, p.x), p.big)
+end
 
 # constructors for doublefloats
 function DoubleFloats.Double16(p::P{T}) where {T <: AbstractFloat}
@@ -20,8 +27,12 @@ end
 function DoubleFloats.Double64(p::P{T}) where {T <: AbstractFloat}
     return convert(Double64, p)
 end
-function DoubleFloats.DoubleFloat(p::P{T}) where {T <: AbstractFloat}
+function DoubleFloats.DoubleFloat(p::P{T}) where {T <: Base.IEEEFloat}
     return convert(DoubleFloat{T}, p)
+end
+function DoubleFloats.DoubleFloat(p::P{T}) where {T <: DoubleFloats.MultipartFloat}
+    # nothing to do, is already a double float
+    return p
 end
 
 end
--- test/doublefloats.jl
+++ test/doublefloats.jl
@@ -1,12 +1,15 @@
 # load DoubleFloatsExt
 using DoubleFloats
+using Random
 
 FLOAT_TYPES = [Float16, Float32, Float64]
 DFLOAT_TYPES = [Double16, Double32, Double64]
 
+RNG = MersenneTwister(0)
+
 @testset "P{$F}" for F in cat(FLOAT_TYPES, DFLOAT_TYPES; dims = 1)
     @testset "DoubleFloat $DF" for DF in DFLOAT_TYPES
-        @testset "convert from PrecisionCarrier{$F} to $DF, for $v" for v in [0.1, 1.0, rand(), rand()]
+        @testset "convert from PrecisionCarrier{$F} to $DF, for $v" for v in [0.1, 1.0, rand(RNG), rand(RNG)]
             # conversion is overloaded to remain at stable PrecisionCarrier types inside calculations
             # but the internal float type is converted
             p = PrecisionCarrier{F}(v)
@@ -20,5 +23,21 @@ DFLOAT_TYPES = [Double16, Double32, Double64]
             @test typeof(pc) == PrecisionCarrier{DF}
             @test isapprox(pc, v; rtol = max(eps(F), eps(DF)))
         end
+    end
+
+    @testset "convert from PrecisionCarrier{$F} to DoubleFloat, for $v" for v in [0.1, 1.0, rand(RNG), rand(RNG)]
+        p = PrecisionCarrier{F}(v)
+        pc = convert(DoubleFloat, p)
+
+        _double(::Type{DoubleFloat{T}}) where {T} = DoubleFloat{T}
+        _double(::Type{T}) where {T} = DoubleFloat{T}
+
+        @test typeof(pc) == PrecisionCarrier{_double(F)}
+        @test isapprox(pc, v; rtol = eps(F))
+
+        # "constructor" call
+        pc = DoubleFloat(p)
+        @test typeof(pc) == PrecisionCarrier{_double(F)}
+        @test isapprox(pc, v; rtol = eps(F))
     end
 end
