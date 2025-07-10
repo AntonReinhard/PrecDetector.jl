29d899bd12d0a2ab5d799c73778a6abe405c918d
AntonReinhard <anton.reinhard@proton.me>
2025-07-10 16:17:04 +0200

    Fix 32bit compat

--- src/functionality/print.jl
+++ src/functionality/print.jl
@@ -1,4 +1,4 @@
-function _print_colored_epsilon(io::IO, ε::Int64)
+function _print_colored_epsilon(io::IO, ε::Integer)
     color = if ε < 10
         :green
     elseif ε < 100
@@ -9,7 +9,7 @@ function _print_colored_epsilon(io::IO, ε::Int64)
         :magenta
     end
 
-    return if (ε == typemax(Int64))
+    return if (ε == typemax(ε))
         printstyled(io, "<ε=Inf>"; color = color)
     else
         printstyled(io, "<ε=$ε>"; color = color)
